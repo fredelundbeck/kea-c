b9bcc1f1203ac1ccba58957e3c3472102009a7d8
Frederik L. Jørgensen <Frederiklundbeck@live.dk>
2020-02-18 18:57:14 +0100

    Add assignment5

--- exercises-1-6/assignment5.c
+++ exercises-1-6/assignment5.c
@@ -1,12 +1,58 @@
 /*
+Make a program where you input a character;
+If the character is an “a” or “A” the program will printout “Apple”;
+If the character is an “b” or “B” the program will printout “Banana”;
+If the character is an “c” or “C” the program will printout “Cherry”;
+If the character is an “d” or “D” the program will printout “Date”;
+If the character is an “e” or “E” the program will printout “Elderberry”;
+Otherwise the program will answer “I don’t know any fruit with a ” the entered letter.
+Use if-else.
 */
 
 #include <stdio.h>
+#include <ctype.h>
+
+void print_fruit(char c);
 
 int main(void)
 {
-    
+
+    print_fruit('A');
+    print_fruit('X');
+
+    return 0;
+}
+
+void print_fruit(char c)
+{
+    if (toupper(c) == 'A')
+    {
+        printf("Apple\n");
+    }
+    else if (toupper(c) == 'B')
+    {
+        printf("Banana\n");
+    }
+    else if (toupper(c) == 'C')
+    {
+        printf("Cherry\n");
+    }
+    else if (toupper(c) == 'D')
+    {
+        printf("Date\n");
+    }
+    else if (toupper(c) == 'E')
+    {
+        printf("Elderberry\n");
+    }
+    else
+    {
+        printf("I don't know any fruit with a %c\n", c);
+    }
 }
 
 /*
+Output:
+Apple
+I don't know any fruit with a X
 */
