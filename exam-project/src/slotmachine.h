07625f6917718e004261e78c957d8eed378cae41
Fliipzy <Frederiklundbeck@live.dk>
2020-05-25 16:34:48 +0200

    Added slotmachine_configuration_t to the slotmachine.h

--- exam-project/src/slotmachine.h
+++ exam-project/src/slotmachine.h
@@ -23,9 +23,20 @@ This struct represents the slotmachine.
 It holds a maximum of 3 wheels.
 */
 typedef struct slotmachine
-{
+{   
+    slotmachine_configuration_t settings;
     wheel_t wheels[3];
 } slotmachine_t;
+
+/*
+Represents the various settings in the slotmachine:
+US dollars to credit conversion, price per spin etc.
+*/
+typedef struct slotmachine_configuration
+{
+    int spin_credit_price;
+    float usd_to_credit_conversion;
+} slotmachine_configuration_t;
 
 /*
 This struct represents the wheel in the slotmachine.
