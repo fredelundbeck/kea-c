0bde75823ec09f3bcc6a880d2c29da37aa6e3dc3
Fliipzy <Frederiklundbeck@live.dk>
2020-05-25 16:18:00 +0200

    Added rule struct to the slotmachine.h

--- exam-project/src/slotmachine.h
+++ exam-project/src/slotmachine.h
@@ -1,6 +1,11 @@
 #ifndef __SLOTMACHINE_H__
 #define __SLOTMACHINE_H__
 
+#pragma region type_definitons
+
+/*
+This enum represents each symbol available in the wheels.
+*/
 typedef enum symbol 
 {
     APPLE,
@@ -13,15 +18,42 @@ typedef enum symbol
     PLUM,
 };
 
+/*
+This struct represents the slotmachine.
+It holds a maximum of 3 wheels.
+*/
 typedef struct slotmachine
 {
     wheel_t wheels[3];
 } slotmachine_t;
 
+/*
+This struct represents the wheel in the slotmachine.
+A wheel holds an array of 10 symbols & an integer
+that tells which symbol is the current one.
+*/
 typedef struct wheel
 {
-    /* data */
+    int current_symbol;
+    symbol symbols[10];
 } wheel_t;
 
+/*
+This struct represents the rules of the slotmachine i.e 
+which order of symbols - when rolled - pays back.
+*/
+typedef struct rule 
+{
+    symbol symbols[3];
+    int price;
+} rule_t;
+
+#pragma endregion type_definitions
+
+#pragma region type_functions
+
+
+
+#pragma endregion type_functions
 
 #endif // __SLOTMACHINE_H__
