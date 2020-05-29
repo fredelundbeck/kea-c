81e36168545fc784a02b7795ad2e32268d09b52e
Frederik <Frederiklundbeck@live.dk>
2020-05-29 23:01:19 +0200

    Added guide comment in top of sketch.ino

--- exam-project/arduino/sketch/sketch.ino
+++ exam-project/arduino/sketch/sketch.ino
@@ -1,3 +1,18 @@
+/*
+
+PLEASE READ BEFORE RUNNING THIS ON YOUR ARDUINO!
+
+Here's some prerequisites to run this on your arduino as intended:
+
+Make sure you have a breadboard with a button circuit connected to
+your arduino (as it is required in order to play), it should be connected with 
+a jumper wire to pin 13.
+
+Full details of the circuit schematics can be found in the .pdf file
+in the doc folder.
+
+*/
+
 #include "game.h"
 #include "utils.h"
 #include "ardsetup.h"
